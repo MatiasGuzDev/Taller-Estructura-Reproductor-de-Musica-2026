a7a481ed230def296754f98a95215748bada7fc4
Martin Canales <martin.ca901@gmail.com>
2026-05-02 23:59:45 -0400

    Parte de la interfaz del menú principal

--- main.cpp
+++ main.cpp
@@ -10,4 +10,29 @@ int main() {
     Reproductor reproductor;
 
     reproductor.cargarArchivos();
+
+    char opcion;
+    do {
+        std::cout << "\n=========================================\n";
+        reproductor.();
+        std::cout << "\n=========================================\n";
+        std::cout << "Opciones:\n";
+        std::cout << "W - Reproducir / Pausar\n";
+        std::cout << "Q - Pista Anterior\n";
+        std::cout << "E - Pista Siguiente\n";
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
