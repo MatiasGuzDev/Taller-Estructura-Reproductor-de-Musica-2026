7ed09325835443b8a775b1d87a527e166de4f4cf
Martin Canales <martin.ca901@gmail.com>
2026-05-02 23:35:20 -0400

    Prueba para ver si funciona el commit bien

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by marti on 02-05-2026.
+//
+
+
+#include <iostream>
+#include <Reproductor.hpp>
+
+int main() {
+    Reproductor reproductor;
+
+    reproductor.cargarArchivos();
+}
